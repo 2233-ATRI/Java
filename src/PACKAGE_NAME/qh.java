90794896ccd2319a11d5b35046bd54674f84de95
wuqihao <191479297@qq.com>
2022-04-16 18:14:03 +0800

    java1

---
+++ src/PACKAGE_NAME/qh.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class qh {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        double a=input.nextDouble(),b = input.nextDouble();
+        double c;
+        c=a%b;
+        System.out.println(c);
+    }
+
+}
