91e8a01ea24ddc833d6536da20ee8f54e09761dd
wuqihao <191479297@qq.com>
2022-04-17 15:50:30 +0800

    java1

---
+++ xunhuanfirst.java
@@ -0,0 +1,11 @@
+import  java.util.Scanner;
+public class xunhuanfirst {
+    public static void main(String[] args){
+        int cout=0;
+        while(cout<100){
+            System.out.println("welcome to java");
+            cout++;
+        }
+    }
+
+}
