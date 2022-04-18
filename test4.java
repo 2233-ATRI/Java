8bc0667b8af274b28d5123b4ec204b665bb269c1
wuqihao <191479297@qq.com>
2022-04-18 22:38:23 +0800

    java1

---
+++ test4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class test4 {
+    public static void main(String[] args) {
+        int list[]={1,2,3,4,5,6,7};
+        for(int i = 1;i< list.length;i++){
+            list[i]=list[i-1];
+        }
+        for(int i = 0; i < list.length;i++){
+            System.out.println(list[i]+" ");
+        }
+    }
+}
